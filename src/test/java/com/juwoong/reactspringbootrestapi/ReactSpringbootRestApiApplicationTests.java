f12dcc514a0e9ac8f7b4f13eff2fca2a409c2099
Juwoong <wwong7247@gmail.com>
2023-07-15 22:54:47 +0900

    프로젝트 초기환경 셋팅

---
+++ src/test/java/com/juwoong/reactspringbootrestapi/ReactSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juwoong.reactspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
