82395fdf83a60d2036cf93ec69bf2899f2b5ebce
Juwoong <wwong7247@gmail.com>
2023-07-19 14:10:37 +0900

    orders 스키마 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS pockets;
 DROP TABLE IF EXISTS content;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS pockets;
 
 CREATE TABLE content
 (
@@ -17,18 +18,28 @@ CREATE TABLE users
     user_id  VARCHAR(200) PRIMARY KEY,
     email    VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    name     VARCHAR(50) NOT NULL,
-
+    name     VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE pockets
 (
-    pocketId     VARCHAR(200) PRIMARY KEY,
-    userId       VARCHAR(200),
-    contentId    VARCHAR(200),
-    contentTitle VARCHAR(255),
-    FOREIGN KEY (userId) REFERENCES users (userId) ON DELETE
-    CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (contentId) REFERENCES content (contentId) ON DELETE
-    CASCADE ON UPDATE CASCADE
+    pocket_id     VARCHAR(200) PRIMARY KEY,
+    user_id       VARCHAR(200) NOT NULL,
+    content_id    VARCHAR(200) NOT NULL,
+    content_title VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE
+        CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (content_id) REFERENCES content (content_id) ON DELETE
+        CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE orders
+(
+    order_id  VARCHAR(200) PRIMARY KEY,
+    user_id   VARCHAR(200) NOT NULL,
+    price     BIGINT       NOT NULL,
+    created_at DATETIME     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE
+        CASCADE ON UPDATE CASCADE
+);
+
